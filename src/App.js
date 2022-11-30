e3f218a9e005d288d30581cbdbdb7bfc78a43331
leegaeddong <hnmg1999@gmail.com>
2022-11-30 21:23:14 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="HeadStyle">
+        <div>to do list</div>
+        <div>react</div>
+      </div>
+      <Second />
     </div>
   );
-}
+};
+
+const Second = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [data, setData] = useState([
+    { id: 1, title: "이건 첫번째입니다 ", content: " 첫번째" },
+    { id: 2, title: "두번째입니다 ", content: "두번째 " },
+  ]);
+  const onDataAdd = () => {
+    let newData = {
+      id: data.length + 1,
+      title: title,
+      content: content,
+    };
+    setData([...data, newData]);
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="SecondStyle">
+        <div className="SecondStyle_1"></div>
+        <span className="SecondStyle-label"> 제목 </span>
+        <input
+          onChange={(event) => setTitle(event.target.value)}
+          type="text"
+          name="title"
+        ></input>
+        <span className="SecondStyle-label"> 내용</span>
+        <input
+          onChange={(event) => setContent(event.target.value)}
+          type="text"
+          name="content"
+        ></input>
+        <button onClick={onDataAdd} className="add-button">
+          추가하기
+        </button>
+      </div>
+      <div>
+        <h2> working..</h2>
+        <div className="todo-box">
+          <h2 className="todo-title"> 리액트를 공부하기</h2>
+          <div>리액트 기초를 공부해봅시다.</div>
+          <div className="buttonAll">
+            <button className="delete-button">삭제하기 </button>
+            <button className="end-button">완료</button>
+          </div>
+        </div>
+        <div>
+          <h2> done..</h2>
+          <div className="todo-box">
+            <h2 className="todo-title"> 리액트를 공부하기</h2>
+            <div>리액트 기초를 공부해봅시다.</div>
+            <div className="buttonAll">
+              <button className="delete-button">삭제하기 </button>
+              <button className="end-button">완료</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
